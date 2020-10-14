5485d85d40961687f2f1fbbd1e30fc7bd59a6250
Anders Kensby <anders.kensby@gmail.com>
2020-10-14 13:59:53 +0200

    test CodeBuild

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Testing the CodeBuild jobs for main and release branches
         </p>
         <a
           className="App-link"
