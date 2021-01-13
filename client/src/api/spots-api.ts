890c0ca00c589acfc603fa78deebc80627dec44e
Anders Kensby <anders.kensby@gmail.com>
2021-01-13 01:06:43 +0100

    add authorizer in backend (working)

--- client/src/api/spots-api.ts
+++ client/src/api/spots-api.ts
@@ -2,12 +2,13 @@ import { apiEndpoint } from '../config'
 import { Spot } from '../types/Spot'
 import Axios from 'axios'
 
-export async function getSpots(): Promise<Spot[]> {
+export async function getSpots(idToken: string): Promise<Spot[]> {
   console.log(`Fetching spots - ${apiEndpoint}/spots`)
 
   const response = await Axios.get(`${apiEndpoint}/spots`, {
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
     },
   })
   return response.data.items
